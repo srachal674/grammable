92655cd919c006f5287186ea9a908927f30a0761
Shannon Rachal <srachal674@gmail.com>
2019-10-06 20:43:56 +0000

    Copied carrerwave.rb code from Nomster and pasted in grammable carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-    config.storage = :fog 
-   # git config.aws_bucket = ENV["AWS_BUCKET"]             
-    config.aws_acl = "public-read"
-  
-    config.aws_credentials = {           
-      access_key_id:      ENV["AWS_ACCESS_KEY"],   
-      secret_access_key:  ENV["AWS_SECRET_KEY"],
-      aws_bucket =        ENV["AWS_BUCKET"],
-      region:             ENV["AWS_REGION"]
+    config.fog_provider = 'fog/aws'               #required
+    config.fog_public = false
+    config.fog_credentials = {
+      provider:               'AWS',                  #required
+      aws_access_key_id:      ENV['AWS_ACCESS_KEY'],   #required
+      aws_secret_access_key:  ENV['AWS_SECRET_KEY'],
     }
+    config.fog_directory = ENV['AWS_BUCKET']
   end
