45214b150259880d1554ae2d16721a72bb95c571
Shannon Rachal <srachal674@gmail.com>
2019-10-06 20:40:57 +0000

    Moved aws_bucket = [AWS_BUCKET] into the { } part of carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,13 @@
 
 CarrierWave.configure do |config|
     config.storage = :fog 
-    config.aws_bucket = ENV["AWS_BUCKET"]             
+   # git config.aws_bucket = ENV["AWS_BUCKET"]             
     config.aws_acl = "public-read"
   
     config.aws_credentials = {           
       access_key_id:      ENV["AWS_ACCESS_KEY"],   
       secret_access_key:  ENV["AWS_SECRET_KEY"],
+      aws_bucket =        ENV["AWS_BUCKET"],
       region:             ENV["AWS_REGION"]
     }
   end
