5c76740d7cfcd082738977a6d5a514fd62182072
Shannon Rachal <srachal674@gmail.com>
2019-10-07 00:17:27 +0000

    Pictures loading without doing steps in lesson 17

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,6 @@
 class Gram < ApplicationRecord
     validates :message, presence: true
-    validates :photos, presence: true
+    validates :photos, presence: false
 
     mount_uploader :photos, PhotosUploader
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
 
     factory :gram do
       message {"hello" }
+
       association :user
     end
 end
