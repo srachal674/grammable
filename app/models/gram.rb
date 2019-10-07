8ee6fa9811dd26ec52afa97635e71351ce2c56cb
Shannon Rachal <srachal674@gmail.com>
2019-10-07 01:02:19 +0000

    Lesson 18 code still 2 failures. Error with gram_id

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,9 +1,11 @@
 class Gram < ApplicationRecord
     validates :message, presence: true
     validates :photos, presence: false
+    has_many :comments
 
     mount_uploader :photos, PhotosUploader
 
 
     belongs_to :user
+   
 end
