afad960eeb338219c4319ffa449704a290f8b5b0
Shannon Rachal <srachal674@gmail.com>
2019-10-08 02:52:15 +0000

    Lesson 18 one error saying the fixtures folder doesn't contain the file picture.png but it does

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -151,7 +151,7 @@ RSpec.describe GramsController, type: :controller do
       post :create, params: {
         gram: {
           message: 'Hello!',
-          photos: fixtures_file_upload("/picture.png", 'image/png')
+          photos: fixture_file_upload("/picture.png", 'image/png')
         }
       }
 
